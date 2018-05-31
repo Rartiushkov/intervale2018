75f19895c9498ab44e8dfec80a1f481e826e0da0
Роман Артюшков <rartiushkov@gmail.com>
2018-06-01 01:01:59 +0300

    new real

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Intervale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var staff = new Staff(); //создаем класс сотрудников
+            ReadWriteStaff.Read(staff); //пробуем загрузить из файла сотрудников
+
+            var viewConsole = new ViewConsole(); //создаем консольное отображение
+            viewConsole.StartMainMenu(staff); // передаем сотрудников туда, для управления
+        }
+    }
+}
